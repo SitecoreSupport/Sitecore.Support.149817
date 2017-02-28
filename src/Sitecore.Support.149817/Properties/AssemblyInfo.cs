302250049360ff56a0bc74d0a258e780c3cd5a3d
DK\ALTU <altu@sitecore.net>
2017-02-28 14:47:37 +0200

    patch 149817 version 0

---
+++ src/Sitecore.Support.149817/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.149817")]
+[assembly: AssemblyProduct("Sitecore.Support.149817")]
+[assembly: ComVisible(false)]
